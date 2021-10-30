a4aef63d8f14cfc617609b96029cbbfa7032eb5d
therokibul <therokibul@gmail.com>
2021-10-30 15:25:04 +0600

    30-10

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  
-  
-  
-
-  
-  
-  
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      
-      
-      
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        
-        child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                height: 200,
+                image: NetworkImage(
+                    'https://cdn.pixabay.com/photo/2021/10/20/07/41/monkey-d-luffy-6725313_960_720.png'),
+              ),
+              Text('Network Image'),
+              Image(
+                height: 200,
+                image: AssetImage('images/cat.jpg'),
+              ),
+              Text('Asset Image')
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
+    ),
+  );
 }
