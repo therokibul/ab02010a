98f5ea621ff895f369c628f9aa7bbac23174a552
therokibul <therokibul@gmail.com>
2021-11-01 15:12:28 +0600

    01-11

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,78 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Column(
-            children: [
-              Image(
-                height: 200,
-                image: NetworkImage(
-                    'https://cdn.pixabay.com/photo/2021/10/20/07/41/monkey-d-luffy-6725313_960_720.png'),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Back',
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+                Text(
+                  'Content',
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                ),
+                Text(
+                  'Filter',
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            Image(
+              image: AssetImage('images/cat.jpg'),
+              height: 200,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(40, 10, 100, 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Header',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-              Text('Network Image'),
-              Image(
-                height: 200,
-                image: AssetImage('images/cat.jpg'),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(40, 10, 100, 10),
+              child: Text(
+                'Labore eu ad esse aliqua occaecat occaecat anim occaecat voluptate aliqua cillum do aliqua.',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
               ),
-              Text('Asset Image')
-            ],
-          ),
+            ),
+            Image(
+              image: AssetImage('images/cat.jpg'),
+              height: 200,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(40, 10, 100, 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Header',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(40, 10, 100, 10),
+              child: Text(
+                'Labore eu ad esse aliqua occaecat occaecat anim occaecat voluptate aliqua cillum do aliqua.',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     ),
