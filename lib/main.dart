87edde97a92b2ae2123e96607cb0d7714f647092
therokibul <therokibul@gmail.com>
2021-11-10 15:13:20 +0600

    10-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.green,
-          title: Text(
-            'Profile',
-            style: TextStyle(fontSize: 40),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage('images/cat.jpg'),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Container(
-                color: Colors.grey,
-                width: 300.0,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Rakib Hossen',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 40),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Settings'),
+                Text(
+                  'Profile',
+                  style: TextStyle(fontSize: 25),
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Container(
-                color: Colors.grey,
-                width: 300.0,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Rakib Hossen',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Container(
-                color: Colors.grey,
-                width: 300.0,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Rakib Hossen',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-            ],
-          ),
+                Text('Logout'),
+              ],
+            ),
+            CircleAvatar(
+              radius: 80,
+              backgroundImage: AssetImage('images/cat.jpg'),
+            ),
+            Text(
+              'Victoria Robertson',
+              style: TextStyle(fontSize: 25),
+            ),
+            Text(
+              'A mantra goes here',
+              style: TextStyle(fontSize: 16),
+            ),
+            ListTile(
+              leading: Image.asset('images/cat.jpg'),
+              title: Text('Header'),
+              subtitle: Text(
+                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
+            ),
+            ListTile(
+              leading: Image.asset('images/cat.jpg'),
+              title: Text('Header'),
+              subtitle: Text(
+                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
+            ),
+            ListTile(
+              leading: Image.asset('images/cat.jpg'),
+              title: Text('Header'),
+              subtitle: Text(
+                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
+            ),
+            ListTile(
+              leading: Image.asset('images/cat.jpg'),
+              title: Text('Header'),
+              subtitle: Text(
+                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
+            ),
+            ListTile(
+              leading: Image.asset('images/cat.jpg'),
+              title: Text('Header'),
+              subtitle: Text(
+                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
+            ),
+          ],
         ),
       ),
     ),
-  );
+  ));
 }
