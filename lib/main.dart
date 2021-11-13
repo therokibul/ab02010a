8e317380e0b0164379a6734c23e772727ba1c292
therokibul <therokibul@gmail.com>
2021-11-13 16:18:04 +0600

    13-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.green,
-      body: Center(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Settings'),
-                Text(
-                  'Profile',
-                  style: TextStyle(fontSize: 25),
-                ),
-                Text('Logout'),
-              ],
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: Column(
+              children: [Image(image: AssetImage('images/cat.jpg'))],
             ),
-            CircleAvatar(
-              radius: 80,
-              backgroundImage: AssetImage('images/cat.jpg'),
-            ),
-            Text(
-              'Victoria Robertson',
-              style: TextStyle(fontSize: 25),
-            ),
-            Text(
-              'A mantra goes here',
-              style: TextStyle(fontSize: 16),
-            ),
-            ListTile(
-              leading: Image.asset('images/cat.jpg'),
-              title: Text('Header'),
-              subtitle: Text(
-                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
-            ),
-            ListTile(
-              leading: Image.asset('images/cat.jpg'),
-              title: Text('Header'),
-              subtitle: Text(
-                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
-            ),
-            ListTile(
-              leading: Image.asset('images/cat.jpg'),
-              title: Text('Header'),
-              subtitle: Text(
-                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
-            ),
-            ListTile(
-              leading: Image.asset('images/cat.jpg'),
-              title: Text('Header'),
-              subtitle: Text(
-                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
-            ),
-            ListTile(
-              leading: Image.asset('images/cat.jpg'),
-              title: Text('Header'),
-              subtitle: Text(
-                  'Minim dolore aute in proident Lorem nisi consequat laborum.'),
-            ),
-          ],
-        ),
-      ),
-    ),
-  ));
+          )),
+    );
+  }
 }
