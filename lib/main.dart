afa188827c59e4796dff306801b64cad3832626c
therokibul <therokibul@gmail.com>
2021-12-01 15:28:18 +0600

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,221 +12,155 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color(0xff292C3E),
-        body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: ListView(
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+String userName = '';
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // ignore: prefer_const_literals_to_create_immutables
                 children: [
-                  Text(
-                    'Home',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
                   Icon(
-                    Icons.notifications_outlined,
+                    Icons.close,
                     color: Colors.white,
                   ),
-                ],
-              ),
-              Container(
-                height: 100,
-                width: 300,
-                margin: EdgeInsets.all(20),
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                decoration: BoxDecoration(
-                  color: Color(0xff3075FA),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 80,
-                      width: 80,
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    Spacer(),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'User Name',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Flutte Developer',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
                   Text(
-                    'Function',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                    'Sign Up',
+                    style: TextStyle(fontSize: 40, color: Colors.white),
                   ),
-                  MaterialButton(
-                    onPressed: () {},
-                    color: Colors.white.withOpacity(0.5),
-                    child: Text(
-                      'Free',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          Icons.android,
-                          color: Color(0xff3DC4EF),
-                        ),
-                        Text(
-                          'AI learning',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          Icons.list,
-                          color: Color(0xff3DC4EF),
-                        ),
-                        Text(
-                          'Cloud',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
+                  Text(
+                    'Login ',
+                    style: TextStyle(fontSize: 25, color: Colors.green),
                   )
                 ],
               ),
               SizedBox(
                 height: 50,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Auto Trading',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  MaterialButton(
-                    onPressed: () {},
-                    color: Colors.white.withOpacity(0.5),
-                    child: Text(
-                      'Offline',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+              Text(
+                'Welcome $userName',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.green,
+                ),
               ),
               SizedBox(
-                height: 25,
+                height: 50,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          Icons.android,
-                          color: Color(0xff3DC4EF),
-                        ),
-                        Text(
-                          'AI learning',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
+              TextField(
+                style: TextStyle(color: Colors.green),
+                onChanged: (value) {
+                  setState(() {
+                    userName = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  labelStyle: TextStyle(
+                    color: Colors.green,
                   ),
-                  Container(
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          Icons.list,
-                          color: Color(0xff3DC4EF),
-                        ),
-                        Text(
-                          'Cloud',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                  hintText: 'Enter your Name',
+                  hintStyle: TextStyle(
+                    color: Colors.green,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.green),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Colors.green,
+                  ),
+                  hintText: 'Enter your Email',
+                  hintStyle: TextStyle(
+                    color: Colors.green,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextField(
+                obscureText: true,
+                style: TextStyle(color: Colors.green),
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                    color: Colors.green,
+                  ),
+                  hintText: 'Enter your Password',
+                  hintStyle: TextStyle(
+                    color: Colors.green,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.green,
+                padding: EdgeInsets.all(10),
+                minWidth: 300,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Forgot your password?',
+                  style: TextStyle(fontSize: 25, color: Colors.green),
+                ),
               )
             ],
           ),
