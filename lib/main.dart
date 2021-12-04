49c3f6859201c5984591dd6fc3bf6553314b7192
therokibul <therokibul@gmail.com>
2021-12-04 15:48:50 +0600

    04-12

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,8 +15,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -26,146 +22,104 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-String userName = '';
-
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  Icon(
-                    Icons.close,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                  Text(
-                    'Login ',
-                    style: TextStyle(fontSize: 25, color: Colors.green),
-                  )
-                ],
+          child: Center(
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage('images/signin.png'),
+              height: 200,
+            ),
+            Text(
+              'Sign In',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Welcome $userName',
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
                 style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.green,
+                  color: Colors.black,
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.green),
-                onChanged: (value) {
-                  setState(() {
-                    userName = value;
-                  });
-                },
                 decoration: InputDecoration(
                   labelText: 'Name',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                  ),
-                  hintText: 'Enter your Name',
-                  hintStyle: TextStyle(
-                    color: Colors.green,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.green),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
                 decoration: InputDecoration(
                   labelText: 'Email',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                  ),
-                  hintText: 'Enter your Email',
-                  hintStyle: TextStyle(
-                    color: Colors.green,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
                 obscureText: true,
-                style: TextStyle(color: Colors.green),
                 decoration: InputDecoration(
                   labelText: 'Password',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                  ),
-                  hintText: 'Enter your Password',
-                  hintStyle: TextStyle(
-                    color: Colors.green,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2.0),
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 50,
-              ),
-              MaterialButton(
-                onPressed: () {},
-                color: Colors.green,
-                padding: EdgeInsets.all(10),
-                minWidth: 300,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Forgot your password?',
-                  style: TextStyle(fontSize: 25, color: Colors.green),
-                ),
-              )
-            ],
-          ),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            )
+          ],
         ),
-      ),
+      )),
     );
   }
 }
