a471df805089c49d8caf5749ddb61215abc721b0
therokibul <therokibul@gmail.com>
2021-12-29 15:58:59 +0600

    h

--- lib/pages/second.dart
+++ lib/pages/second.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Second extends StatelessWidget {
-  const Second({ Key? key }) : super(key: key);
+class Second extends StatefulWidget {
+  Second({Key? key}) : super(key: key);
+
+  @override
+  State<Second> createState() => _SecondState();
+}
+
+class _SecondState extends State<Second> {
+  bool animationValue = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.red,
-      child: Text(
-          "Second page",
-          textScaleFactor: 3,
-        ),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: Duration(seconds: 3),
+              height: animationValue ? 200 : 300,
+              width: animationValue ? 200 : 300,
+              decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius:
+                      BorderRadius.circular(animationValue ? 20 : 150)),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    animationValue = !animationValue;
+                  });
+                },
+                child: Text('Animation Update'))
+          ],
+        ),
+      ),
     );
   }
 }
